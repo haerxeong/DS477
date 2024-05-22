f2c225e6d6966996bad08150e40eb7e0d6349a0b
haerxeong <haeryeong04@naver.com>
2024-05-22 20:35:24 +0900

    Initial Commit

---
+++ Task2_List_by_array/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Task2_List_by_array
+//
+//  Created by 이해령 on 5/22/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
